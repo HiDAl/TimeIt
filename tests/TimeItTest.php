2c37e0659cf7b477b97a9d3937f84b3ef8bf3dac
Pablo Alcantar <hidal@radiohead.cl>
2017-03-26 15:13:56 +0200

    finishing tests

--- tests/TimeItTest.php
+++ tests/TimeItTest.php
@@ -67,6 +67,71 @@ class TimeItTest extends \PHPUnit_Framework_TestCase {
     $this->assertLessThanOrEqual(2000, $total);
   }
 
+  public function testStopTime() {
+    $time = $this->constructMockTimeIt();
+
+    $elapsed = $time->stop();
+
+    $this->assertGreaterThanOrEqual(1000, $elapsed);
+    $this->assertLessThanOrEqual(2000, $elapsed);
+  }
+
+  /**
+   * @expectedException HiDAl\TimeIt\Exception
+   */
+  public function testStartThrowExceptionIfStopped() {
+    $time = new TimeIt;
+
+    $elapsed = $time->stop();
+
+    $this->expectException($time->elapsed());
+  }
+
+  public function testInstanstEqualZero() {
+    $time  = new TimeIt;
+
+    $instants = $time->instants();
+
+    $this->assertEquals(0, count($instants));
+  }
+
+  public function testInstanstMoreThanOne() {
+    $time  = $this->constructMockTimeIt();
+    $times = 4;
+
+    for ($i = 0; $i < $times; $i++) {
+      $time->elapsed();
+    }
+
+    $instants = $time->instants();
+
+    $this->assertEquals($times, count($instants));
+  }
+
+  /**
+   * @expectedException HiDAl\TimeIt\Exception
+   */
+  public function testPlzBadArguments() {
+    $this->expectException(TimeIt::plz(null));
+  }
+
+  public function testPlzWaitOneSecondAndReturnTrue() {
+    $lambda = function() {
+      sleep(1);
+      return true;
+    };
+
+    $return = TimeIt::plz($lambda);
+
+    $this->assertInternalType('array', $return);
+
+    list($elapsed, $returnValue) = $return;
+
+    $this->assertGreaterThanOrEqual(1000, $elapsed);
+    $this->assertLessThanOrEqual(2000, $elapsed);
+
+    $this->assertTrue($returnValue);
+  }
 
   // Mock Object for simulate 1 seconds of sleep between calls
   private function constructMockTimeIt() {
